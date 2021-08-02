cc4c432a1d923139859a0d1d7c58738ce64b3e51
vaki777 <59484434+vaki777@users.noreply.github.com>
2021-08-02 14:38:33 +0200

    Added simple select commands.

---
+++ filter.sql
@@ -0,0 +1,9 @@
+/* return all data from actor table
+SELECT * FROM actor;
+
+/* return specific columns
+SELECT first_name, last_name, date_of_birth FROM actor;
+
+/* return sorted date
+SELECT * FROM actor 
+ORDER BY date_of_birth, net_worth_in_milions;
