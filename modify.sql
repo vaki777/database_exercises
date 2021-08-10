0c68634b1a815d485c1ccc5040b8f234cf3b8b81
vaki777 <59484434+vaki777@users.noreply.github.com>
2021-08-10 15:30:41 +0200

    Added delete and update commands.

---
+++ modify.sql
@@ -0,0 +1,5 @@
+/* delete from table
+DELETE FROM actor WHERE first_name='John' AND last_name='Doe';
+
+/* increase net worth
+UPDATE actor SET net_worth_in_milions=70 WHERE net_worth_in_milions<70;
